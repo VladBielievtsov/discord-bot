bfafe4d4209174c3725ab5b5773c32cf10a5a3f6
VladBielievtsov <vladbielievtsov@gmail.com>
2024-06-17 16:15:25 +0300

    updates

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"fmt"
-	"go-discord-bot/db"
 	"go-discord-bot/handlers"
 	"go-discord-bot/utils"
-	"math/rand"
 	"os"
 	"os/signal"
-	"strings"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
@@ -17,98 +14,45 @@ import (
 	_ "github.com/lib/pq"
 )
 
-var images = [8]string{
-	"https://i.pinimg.com/736x/e0/e2/75/e0e2751ec0a039f536234e3f87432acf.jpg",
-	"https://i.pinimg.com/564x/b0/ff/44/b0ff44d634233716697b774da9a4ad7a.jpg",
-	"https://i.pinimg.com/736x/fc/9b/c0/fc9bc08b6313c642965c491b94010e75.jpg",
-	"https://i.pinimg.com/736x/4e/56/d8/4e56d8cebeceeab0fc5fe2fad198d6ba.jpg",
-	"https://i.pinimg.com/564x/bf/6f/8a/bf6f8ab7b14f71af53aa226c40efdb86.jpg",
-	"https://i.pinimg.com/564x/4f/c2/45/4fc24581a7e7349e4551ad1924918d15.jpg",
-	"https://i.pinimg.com/564x/bd/26/b3/bd26b3b9772adb0ac0854cef3ebd16bf.jpg",
-	"https://i.pinimg.com/564x/e9/f8/dc/e9f8dc62e3ad3053ca9aa195edd72016.jpg",
-}
-
-const prefix string = ".cat"
-
 func main() {
 	err := godotenv.Load()
 	utils.ErrorHandler(err)
 
 	token := os.Getenv("BOT_TOKEN")
+	if token == "" {
+		fmt.Println("BOT_TOKEN is not set in the environment variables")
+		return
+	}
+
 	sess, err := discordgo.New("Bot " + token)
 	utils.ErrorHandler(err)
-
-	db.ConnectDatabase()
-	defer db.DB.Close()
-
-	sess.AddHandler(func(s *discordgo.Session, r *discordgo.MessageReactionAdd) {
-		if r.Emoji.Name == "🔥" {
-			s.GuildMemberRoleAdd(r.GuildID, r.UserID, "1251984426032824400")
-			s.ChannelMessageSend(r.ChannelID, fmt.Sprintf("%v has been added to %v", r.UserID, r.Emoji.Name))
-		}
-	})
-
-	sess.AddHandler(func(s *discordgo.Session, r *discordgo.MessageReactionRemove) {
-		if r.Emoji.Name == "🔥" {
-			s.GuildMemberRoleRemove(r.GuildID, r.UserID, "1251984426032824400")
-			s.ChannelMessageSend(r.ChannelID, fmt.Sprintf("%v has been removed from %v", r.UserID, r.Emoji.Name))
-		}
-	})
-
-	sess.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
-		if m.Author.ID == s.State.User.ID {
-			return
-		}
-
-		// DM Logic
-		if m.GuildID == "" {
-			handlers.UserPromptResponseHandler(s, m)
-		}
-
-		// Server Logic
-
-		args := strings.Split(m.Content, " ")
-
-		if len(args) < 2 && args[0] == prefix {
-			s.ChannelMessageSend(m.ChannelID, "Enter `.cat help` to see all commands")
-			return
-		}
-
-		if args[0] != prefix {
-			return
-		}
-
-		if args[1] == "hello" {
-			handlers.HelloWorldHandler(s, m)
-		}
-
-		if args[1] == "proverbs" {
-			handlers.ProverbsHandler(s, m)
-		}
-
-		if args[1] == "prompt" {
-			handlers.UserPromptHandler(s, m)
-		}
-
-		if args[1] == "answers" {
-			handlers.AnswersHandler(s, m)
-		}
-
-		if args[1] == "rand" {
-			randImage := rand.Intn(len(images))
-			s.ChannelMessageSend(m.ChannelID, images[randImage])
-		}
-
-		if args[1] == "help" {
-			handlers.HelpHandler(s, m)
-		}
-	})
-
-	sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
 
 	err = sess.Open()
 	utils.ErrorHandler(err)
 	defer sess.Close()
+
+	commands := []*discordgo.ApplicationCommand{
+		{
+			Name:        "hello",
+			Description: "Say hello",
+		},
+	}
+
+	guildID := os.Getenv("Guild_ID")
+
+	for _, v := range commands {
+		_, err := sess.ApplicationCommandCreate(sess.State.User.ID, guildID, v)
+		utils.ErrorHandler(err)
+	}
+
+	sess.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		switch i.ApplicationCommandData().Name {
+		case "hello":
+			handlers.HelloHandler(s, i)
+		}
+	})
+
+	sess.Identify.Intents = discordgo.IntentsAllWithoutPrivileged
 
 	fmt.Println("The bot is Online! Press CTRL+C to exit.")
 
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,176 +1,19 @@
 package handlers
 
 import (
-	"encoding/json"
-	"go-discord-bot/db"
+	"fmt"
 	"go-discord-bot/utils"
-	"math/rand"
-	"strconv"
-	"strings"
 
 	"github.com/bwmarrin/discordgo"
 )
 
-type Answers struct {
-	OriginChannelID string
-	FavFood         string
-	FavGame         string
-	RecordID        int64
-}
-
-func (a *Answers) ToMessageEmbed() discordgo.MessageEmbed {
-	fields := []*discordgo.MessageEmbedField{
-		{
-			Name:  "Favorite food",
-			Value: a.FavFood,
+func HelloHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: fmt.Sprintf("Hello %v", i.Member.User.Username),
 		},
-		{
-			Name:  "Favorite game",
-			Value: a.FavGame,
-		},
-		{
-			Name:  "Record ID",
-			Value: strconv.FormatInt(a.RecordID, 10),
-		},
-	}
-
-	return discordgo.MessageEmbed{
-		Title:  "New responses!",
-		Fields: fields,
-	}
-}
-
-var Responses map[string]Answers = map[string]Answers{}
-
-func UserPromptResponseHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
-	answers, ok := Responses[m.ChannelID]
-	if !ok {
-		return
-	}
-
-	if answers.FavFood == "" {
-		answers.FavFood = m.Content
-
-		s.ChannelMessageSend(m.ChannelID, "Great! What's your favorite game now? 🎮")
-		Responses[m.ChannelID] = answers
-		return
-	} else {
-		answers.FavGame = m.Content
-
-		jbytes, err := json.Marshal(answers)
-		utils.ErrorHandler(err)
-		lastInserted, err := db.AddDiscordMessages(jbytes, m.ChannelID)
-		utils.ErrorHandler(err)
-		answers.RecordID = lastInserted
-
-		s.ChannelMessageSend(m.ChannelID, "Great! Thanks you! 😊")
-		embed := answers.ToMessageEmbed()
-		s.ChannelMessageSendEmbed(answers.OriginChannelID, &embed)
-
-		delete(Responses, m.ChannelID)
-	}
-}
-
-func AnswersHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
-	spl := strings.Split(m.Content, " ")
-
-	if len(spl) < 3 {
-		s.ChannelMessageSend(m.ChannelID, "an ID must be provided. Ex: `!gobot answers 1`")
-		return
-	}
-
-	id, err := strconv.Atoi(spl[2])
+	})
 	utils.ErrorHandler(err)
-
-	var recordID int64
-	var answersStr string
-	var userID int64
-
-	query := "SELECT * FROM discord_messages WHERE id = $1"
-	row := db.DB.QueryRow(query, id)
-	err = row.Scan(&recordID, &answersStr, &userID)
-	utils.ErrorHandler(err)
-
-	var answers Answers
-	err = json.Unmarshal([]byte(answersStr), &answers)
-	utils.ErrorHandler(err)
-
-	answers.RecordID = recordID
-	embed := answers.ToMessageEmbed()
-	s.ChannelMessageSendEmbed(m.ChannelID, &embed)
-}
-
-func HelloWorldHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
-	s.ChannelMessageSend(m.ChannelID, "Hello World!")
-}
-
-func ProverbsHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
-	proverbs := []string{
-		"Don't communicate by sharing memory, share memory by communicating.",
-		"Concurrency is not parallelism.",
-		"Channels orchestrate; mutexes serialize.",
-		"The bigger the interface, the weaker the abstraction.",
-		"Make the zero value useful.",
-		"interface{} says nothing.",
-		"Gofmt's style is no one's favorite, yet gofmt is everyone's favorite.",
-		"A little copying is better than a little dependency.",
-		"Syscall must always be guarded with build tags.",
-		"Cgo must always be guarded with build tags.",
-		"Cgo is not Go.",
-		"With the unsafe package there are no guarantees.",
-		"Clear is better than clever.",
-		"Reflection is never clear.",
-		"Errors are values.",
-		"Don't just check errors, handle them gracefully.",
-		"Design the architecture, name the components, document the details.",
-		"Documentation is for users.",
-		"Don't panic.",
-	}
-
-	selection := rand.Intn(len(proverbs))
-
-	author := discordgo.MessageEmbedAuthor{
-		Name: "Rob Pike",
-		URL:  "https://go-proverbs.github.io",
-	}
-	embed := discordgo.MessageEmbed{
-		Title:  proverbs[selection],
-		Author: &author,
-	}
-
-	s.ChannelMessageSendEmbed(m.ChannelID, &embed)
-}
-
-func UserPromptHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
-	channel, err := s.UserChannelCreate(m.Author.ID)
-	utils.ErrorHandler(err)
-
-	if _, ok := Responses[channel.ID]; !ok {
-		Responses[channel.ID] = Answers{
-			OriginChannelID: m.ChannelID,
-			FavFood:         "",
-			FavGame:         "",
-		}
-		s.ChannelMessageSend(channel.ID, "Hey there! Here are some questions")
-		s.ChannelMessageSend(channel.ID, "What's your favorite food?")
-	} else {
-		s.ChannelMessageSend(channel.ID, "We're still waiting... 😅")
-	}
-}
-
-func HelpHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
-	fields := []*discordgo.MessageEmbedField{
-		{
-			Name:  "Everyone commands",
-			Value: "`help` `rand` `hello` `proverbs` `prompt` `answers`",
-		},
-	}
-
-	embed := discordgo.MessageEmbed{
-		Title:  "Help Command",
-		Fields: fields,
-		Color:  1,
-	}
-
-	s.ChannelMessageSendEmbed(m.ChannelID, &embed)
+	// s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Hello %v", m.Author.Username))
 }
